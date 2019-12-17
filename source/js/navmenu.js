1f6037ed8514eeba38726fbd2a069ea8551c7097
Your Name <marina7953042@yandex.ru>
2019-12-17 19:08:27 +0500

    Прописаны теги picher для всех контентных изображений, сделан спрайт из svg. проведена автоматизация

--- source/js/navmenu.js
+++ source/js/navmenu.js
@@ -12,3 +12,36 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+/*
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938631, 30.323055],
+          zoom: 17
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Кэт Энерджи',
+          balloonContent: ''
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/map-pin.png',
+          // Размеры метки.
+          iconImageSize: [124, 106],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-63, -103]
+      });
+
+  myMap.geoObjects
+    .add(myPlacemark);
+});*/
