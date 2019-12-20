816c06343905667a773976d48e3a80ea0850abfd
Your Name <marina7953042@yandex.ru>
2019-12-20 13:00:25 +0500

    Небольшие правки

--- source/js/navmenu.js
+++ source/js/navmenu.js
@@ -1,40 +1,40 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
 
-/*ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
+ymaps.ready(function () {
+  var myMap = new ymaps.Map("map", {
           center: [59.938631, 30.323055],
           zoom: 17
       }, {
-          searchControlProvider: 'yandex#search'
+          searchControlProvider: "yandex#search"
       }),
 
       // Создаём макет содержимого.
       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+          "<div style='color: #FFFFFF; font-weight: bold;'>$[properties.iconContent]</div>"
       ),
 
       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          hintContent: 'Кэт Энерджи',
-          balloonContent: ''
+          hintContent: "Кэт Энерджи",
+          balloonContent: ""
       }, {
           // Опции.
           // Необходимо указать данный тип макета.
-          iconLayout: 'default#image',
+          iconLayout: "default#image",
           // Своё изображение иконки метки.
-          iconImageHref: 'img/map-pin.png',
+          iconImageHref: "img/map-pin.png",
           // Размеры метки.
           iconImageSize: [124, 106],
           // Смещение левого верхнего угла иконки относительно
@@ -44,4 +44,5 @@ navToggle.addEventListener('click', function() {
 
   myMap.geoObjects
     .add(myPlacemark);
-});*/
+});
+
